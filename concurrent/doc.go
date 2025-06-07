34bb5f1716e589cd3f96cadc6bed36d81f9da792
Richard Carson Derr <cakub6@gmx.com>
2025-06-07 21:48:34 +0000

    feat(issue-3): implement concurrent cache

---
+++ concurrent/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 vscode
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Package concurrent provides common concurrent safe types.
+package concurrent
